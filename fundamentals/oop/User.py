1ab61d691a2e6c3882f5f20bd4488cb768787807
Jeff Steele <steelem81@gmail.com>
2022-08-09 17:51:37 -0600

    update User.py to chain methods

--- fundamentals/oop/User.py
+++ fundamentals/oop/User.py
@@ -9,6 +9,7 @@ class user():
     
     def display_info(self):
         print(f"User: {self.first_name} {self.last_name}\n Email: {self.email}\n Age: {self.age}\n Is rewards member: {self.is_rewards_member}\n Gold Card Points:{self.gold_card_points}")
+        return self
 
     def enroll(self):
         if self.is_rewards_member == True:
@@ -16,25 +17,19 @@ class user():
         else:
             self.is_rewards_member = True
             self.gold_card_points = 200
-    
+        return self
+
     def spend_points(self, amount):
         if self.gold_card_points - amount >= 0:
             self.gold_card_points -= amount
         else:
             print("Not enough points to spend")
+        return self
 
-user1 = user('Bruce', 'Wayne', 'iamnotbatman@gmail.com', 62)
-user1.display_info()
-user1.enroll()
-user1.display_info()
+user1,user2, user3  = user('Bruce', 'Wayne', 'iamnotbatman@gmail.com', 62),user('Jason', 'Todd', 'jokersbiggestfan@aol.com', 36), user('Damion', 'Wayne', 'bookytheclown@hotmasil.com', 17)
+user1.display_info().enroll().spend_points(50).display_info().enroll()
+user2.enroll().spend_points(80).display_info()
+user3.display_info().spend_points(40)
 
-user2, user3 = user('Jason', 'Todd', 'jokersbiggestfan@aol.com', 36), user('Damion', 'Wayne', 'bookytheclown@hotmasil.com', 17)
-user1.spend_points(50)
-user2.enroll()
-user2.spend_points(80)
-user1.display_info()
-user2.display_info()
-user3.display_info()
-user1.enroll()
-user3.spend_points(40)
+
 
